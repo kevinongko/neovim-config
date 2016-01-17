8e78345d6396a42e115f98af26d8ca84f3f50b6f
Kevin Ongko <kevin.ongko@gmail.com>
2016-01-17 14:50:49 +0700

    add more condition

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,7 @@
 # Variables
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".zshrc .vimrc"        # list of files/folders to symlink in homedir
-
-# install zsh
-echo "installing zsh"
-sudo apt-get install zsh
-echo "...done"
-
-# install vim-plug
-echo "installing vim-plug"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "...done"
+files=".zshrc .vimrc"             # list of files/folders to symlink in homedir
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -37,15 +26,35 @@ for file in $files; do
     ln -s $dir/$file ~/$file
 done
 
+# install vim-plug
+read -r -p "Install vim-plug? [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY])
+        echo "installing vim-plug"
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        echo "...done"
+        ;;
+    *)
+esac
+
+# install zsh
+read -r -p "Install zsh? [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY])
+        echo "installing zsh"
+        sudo apt-get install zsh
+        echo "...done"
+        ;;
+    *)
+esac
+
 # install oh-my-zsh
-read -r -p "Install oh-my-zsh ? [y/N] " response
+read -r -p "Install oh-my-zsh? the installer will replace your current .zshrc [y/N] " response
 case $response in
     [yY][eE][sS]|[yY])
         echo "installing oh-my-zsh"
         sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-        echo "replacing default .zshrc"
-        mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
-        echo ".zshrc is now update"
         ;;
     *)
         echo "all done, move along"
