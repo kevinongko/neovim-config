d34024f1569830619f5a2a188dbbf1ac0bef8d98
Kevin Ongko <kevin.ongko@gmail.com>
2016-01-17 13:29:28 +0700

    remove update, add some echo"

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,18 +3,21 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files=".zshrc .vimrc"        # list of files/folders to symlink in homedir
 
-# update & upgrade package
-sudo apt-get update && sudo apt-get upgrade
-
 # install zsh
+echo "installing zsh"
 sudo apt-get install zsh
+echo "...done"
 
 # install oh-my-zsh
+echo "installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "...done"
 
 # install vim-plug
+echo "installing vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "...done"
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
